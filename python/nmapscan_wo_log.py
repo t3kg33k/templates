1a506abf3a48e0a2537bc40284a9ba6f4933a67b
ed <ecrosby1971@pm.me>
2022-02-21 18:38:43 -0500

    update from home desktop

--- python/nmapscan_wo_log.py
+++ python/nmapscan_wo_log.py
@@ -27,20 +27,20 @@ from datetime import datetime
 
 # === 2/15/2022, simbleau ===
 regex_mac_exclusions = []
-# regex_mac_exclusions.append("00:00:5e:00:53:af")  -- A single MAC address
-regex_mac_exclusions.append("^00:.*")        ##     -- All MACs starting with '00:'
+# regex_mac_exclusions.append("00:00:5e:00:53:af")
+regex_mac_exclusions.append("^52:.*")
+#regex_mac_exclusions.append("^52:54:00:.*")
 # ===========================
 
 # variable containing the filepath of the nmap scan results file
-mac_list = os.environ['HOME'] + "/maclist.txt"
+mac_list = os.environ['HOME'] + "/bin/maclist.txt"
 
 # variable containing the filepath of the approved mac addresses on the LAN file
-masterfile = os.environ['HOME'] + "/master_mac.txt"
+masterfile = os.environ['HOME'] + "/bin/master_mac.txt"
 
 # If a file from previous nmap scans exists, create a backup of the file
 if os.path.exists(mac_list):
-    shutil.copyfile(mac_list, os.environ['HOME'] + '/maclist_' +
-                    datetime.now().strftime("%Y_%m_%H:%M") + '.log.bk')
+    shutil.copyfile(mac_list, os.environ['HOME'] + '/bin/nmapscans/maclist_' + datetime.now().strftime("%Y_%m_%H%M") + '.log.bk')
 
 # Open a new file for the new nmap scan results
 f = open(mac_list, "w+")
@@ -94,13 +94,18 @@ for i in mac_addresses:
     dic = eval(i)
     # Compare mac address portion of dictionary to mac addresses in the master_mac_addresses file
     # If the scanned mac address is not in the master_mac_addresses file
+    #if dic['mac'] not in master_mac_addresses:
 
     # === 2/15/2022, simbleau ===
     mac_addr = dic['mac']
     # Check if mac address is excluded by regex
+    matched = False
     for pattern in regex_mac_exclusions:
         if re.match(pattern, mac_addr) is not None:
-            continue  # Match found, skip to next MAC
+            matched = True
+            break
+    if matched:
+        continue
     # Check if known MAC in master list
     if mac_addr not in master_mac_addresses:
         # Add scanned mac address to new devices list
@@ -110,8 +115,7 @@ for i in mac_addresses:
 # If the new_devices list isn't empty
 if len(new_devices) != 0:
     # output a warning to the console
-    warning = "\nWARNING!! NEW DEVICE(S) ON THE LAN!! - UNKNOWN MAC ADDRESS(ES): " + str(
-        new_devices) + "\n"
+    warning = "\nWARNING!! NEW DEVICE(S) ON THE LAN!! - UNKNOWN MAC ADDRESS(ES): " + str(new_devices) + "\n"
     print(warning)
 
     # Create email notification of the warning
@@ -121,7 +125,7 @@ if len(new_devices) != 0:
         # content of email
         content = "New unknown device(s) on the LAN: " + str(new_devices)
         # shell process of sending email with mutt
-        m1 = subprocess.Popen('echo "{content}" | mail -s "{subject}" your@email.com'.format(
+        m1 = subprocess.Popen('echo "{content}" | mail -s "{subject}" ecrosby1971@gmail.com'.format(
             content=content, subject=subject), shell=True)
         # output whether email was successful in sending or not
         print(m1.communicate())
